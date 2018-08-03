7093e01a6c36e258f7ed2cac45619398a98bfedb
Stuart <stuart.chinery@gmail.com>
2018-08-03 11:40:54 +0100

    Updated 1Password

--- apps/install.sh
+++ apps/install.sh
@@ -61,11 +61,11 @@ install_mas_apps() {
   brew install mas
 
   echo -e "\033[1;32mInstalling mas apps...\033[0m"
+  mas install 1333542190 # 1Password 7 (7.0.4)
   mas install 405399194 # Kindle (1.21.1)
   mas install 406056744 # Evernote (7.1)
   mas install 410628904 # Wunderlist (3.4.8)
   mas install 425424353 # The Unarchiver (3.11.5)
-  mas install 443987910 # 1Password (6.8.8)
   mas install 497799835 # Xcode (9.3.1)
   xcode-select --install
   echo -e "\033[1;32mFinished installing mas apps\033[0m"
