5a6ff1d156429831a2bfb88cf3b7536d210b26f5
Stuart <stuart.chinery@gmail.com>
2019-06-03 09:26:48 +0100

    Switched to VS Code

---
+++ apps/code.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+echo -e "\033[1;32mInstalling VS Code...\033[0m"
+brew cask install visual-studio-code
+
+brew install duti
+duti -s com.microsoft.VSCode .css all
+duti -s com.microsoft.VSCode .js all
+duti -s com.microsoft.VSCode .jsx all
+duti -s com.microsoft.VSCode .md all
+duti -s com.microsoft.VSCode .txt all
+echo -e ""
