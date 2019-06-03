9b1e1ddd1928115541ae011eaa7b82225a7d7868
Stuart <stuart.chinery@gmail.com>
2019-06-03 09:54:59 +0100

    Added Flutter

---
+++ dev/flutter.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+echo -e "\033[1;32mInstalling Flutter iOS deps...\033[0m"
+brew update
+brew install --HEAD usbmuxd
+brew link usbmuxd
+brew install --HEAD libimobiledevice
+brew install ideviceinstaller
+brew install ios-deploy
+brew install cocoapods
+pod setup
+echo -e "";
