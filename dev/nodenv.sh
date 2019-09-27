5a20900de711c95acffa499286ce98550d2e9c84
Stuart <stuart.chinery@gmail.com>
2019-09-27 08:20:59 +0100

    Manual install of Yarn as via Brew installs Node which isn't needed

--- dev/nodenv.sh
+++ dev/nodenv.sh
@@ -2,7 +2,7 @@
 
 set -euf -o pipefail
 
-NODE_VERSION=10.15.0
+NODE_VERSION=10.16.3
 
 echo -e "\033[1;32mInstalling nodenv and Node $NODE_VERSION...\033[0m"
 brew install nodenv
@@ -10,8 +10,7 @@ eval "$(nodenv init -)"
 nodenv install -f $NODE_VERSION
 nodenv global $NODE_VERSION
 nodenv local $NODE_VERSION
+curl -o- -L https://yarnpkg.com/install.sh | bash
 # TODO: install some global npm packages like jshint
-# TODO: test yarn with reinstall
-# brew install yarn --without-node
-brew install yarn --without-node
 echo -e "";
+ 
